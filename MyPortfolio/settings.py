db18383e2d40f26f3d3be7bfb4c0221c77319ad4
James Mathenge <mathengejames64@gmail.com>
2025-07-21 18:45:22 +0300

    .

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -118,12 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # used only for collectstatic
+
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
 
 
 
